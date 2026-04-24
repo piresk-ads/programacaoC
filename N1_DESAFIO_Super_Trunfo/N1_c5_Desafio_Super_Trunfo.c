c2695edf90fc4be09955c90b716a7cf623513329
piresk-ads <carol.pires.ads@gmail.com>
2026-04-24 22:21:36 +0000

    Iniciei o N1_c5_Desafio_Super_Trunfo

---
+++ N1_DESAFIO_Super_Trunfo/N1_c5_Desafio_Super_Trunfo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+
+ /*
+   Carta 1
+   Estado (char): Bahia
+   Código (char[]): A01
+   Nome da Cidade (char[]): Salvador
+   População (int): 12345678
+   Área (float): 1234.56 km²
+   PIB (float): 8765.54 bilhões de reais
+   Número de Pontos Turísticos (int): 90
+
+
+
+    printf("%formato1 %formato2 %formato3", variavel1, variavel2. variavel3)
+
+   %d: Imprime um inteiro no formato decimal.
+   %i: Equivale a %d.
+   %f: Imprime um número de ponto flutuante no formato padrão.
+   %e: Imprime um número de ponto flutuante na notação científica.
+   %c: Imprime um único caractere.
+   %s: Imprime uma cadeia (string) de caracteres.
+
+   */
+
+
+
+
+}
